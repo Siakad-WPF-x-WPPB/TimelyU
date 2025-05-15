3f0f5898d56ef4fdac586c176a11f8fd1f0a0b8e
eidoxy <adriansondang@gmail.com>
2025-05-15 14:16:17 +0700

    feat(navigation): Implement routing and binding for Home screen

---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+import './app_routes.dart';
+
+import '../modules/home/home_screen.dart';
+// import '../modules/schedule/schedule_screen.dart';
+// import '../modules/task/task_screen.dart';
+// import '../modules/frs/frs_screen.dart';
+
+import '../modules/home/home_binding.dart';
+// import '../modules/schedule/schedule_binding.dart';
+// import '../modules/task/task_binding.dart';
+// import '../modules/frs/frs_binding.dart';
+
+class AppPages {
+  static final routes = [
+    GetPage(
+      name: AppRoutes.home,
+      page: () => HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: AppRoutes.root,
+      page: () => HomeView(),
+      binding: HomeBinding(),
+    ),
+  ];
+}
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,7 @@
+abstract class AppRoutes {
+  static const root = '/';
+  static const home = '/home';
+  static const schedule = '/schedule';
+  static const tasks = '/tasks';
+  static const frs = '/frs';
+}
